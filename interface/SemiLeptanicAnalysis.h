fa4ddb93273b5d8bc4f1cbc365a03364463fff33
Alpha <jui-fa.tsai@cern.ch>
2015-05-12 11:14:14 +0200

    Start to build codes

---
+++ interface/SemiLeptanicAnalysis.h
@@ -0,0 +1,78 @@
+#ifndef HADRONICANALYSIS_H 
+#define HADRONICANALYSIS_H 
+
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <string>
+
+// Root headers 
+#include <TChain.h>
+#include <TTree.h>
+#include <TH1D.h>
+
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+
+#include "TTBarCPV/TTBarCPVAnalysisRun1/interface/TH1Info.h"
+#include "TTBarCPV/TTBarCPVAnalysisRun1/interface/TH1InfoClass.h"
+//
+// class declaration
+//
+
+class SemiLeptanicAnalysis : public edm::EDAnalyzer{
+	public:
+		explicit SemiLeptanicAnalysis(const edm::ParameterSet&);
+		~SemiLeptanicAnalysis();
+
+		static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+
+	private:
+		virtual void beginJob();
+		virtual void analyze(const edm::Event&, const edm::EventSetup&);
+		virtual void endJob();
+
+		// ----------member data ---------------------------
+
+		//// Configurables 
+
+		int                             maxEvents_; 
+		const int                       reportEvery_; 
+		const std::string               inputTTree_;
+		const std::vector<std::string>  inputFiles_;
+		bool debugTH1InfoClass_;
+
+		edm::Service<TFileService> fs;
+		TChain*            chain_;
+ 
+		//const double jetPtMin_;
+		//const double bJetPtMin_;
+		//const double bJetCSVDiscMin_;
+		//const double bJetCSVDiscMax_;
+
+		//const edm::ParameterSet jetSelParams_ ; 
+		//const edm::ParameterSet evtSelParams_ ; 
+
+		//EvtInfoBranches    EvtInfo;
+		//VertexInfoBranches VtxInfo;
+		//GenInfoBranches    GenInfo;
+		//JetInfoBranches    GenJetInfo;
+		//JetInfoBranches    JetInfo;
+		//LepInfoBranches    LepInfo;
+
+		TH1InfoClass<TH1D> h1;
+
+		//bool   McFlagana;
+		//int RunNo;
+		//long int EvtNo;
+		//int LumiNo;
+
+};
+
+#endif
---
+++ test/semiLeptanicAnalysis_cfg.py
@@ -0,0 +1,58 @@
+import FWCore.ParameterSet.Config as cms
+
+from FWCore.ParameterSet.VarParsing import VarParsing
+
+from inputFiles_cfi import * 
+
+options = VarParsing('python')
+
+options.register('outFilename', 'SemiLeptanicAnalysis.root',
+	VarParsing.multiplicity.singleton,
+	VarParsing.varType.string,
+	"Output file name"
+	)
+options.register('MaxEvents', -1,
+	VarParsing.multiplicity.singleton,
+	VarParsing.varType.int,
+	"Run events max"
+	)
+options.register('reportEvery', 1000,
+	VarParsing.multiplicity.singleton,
+	VarParsing.varType.int,
+	"Report every N events (default is N=1000)"
+	)
+options.register('ttreedir', 'ntuple',
+	VarParsing.multiplicity.singleton,
+	VarParsing.varType.string,
+	"Name of ROOT TTree dir: Either 'ntuple' or 'skim' or 'bVeto'"
+	)
+#options.register('JetPtMin', 50.,
+#    VarParsing.multiplicity.singleton,
+#    VarParsing.varType.float,
+#    "Minimum b jet Pt"
+#    )
+#options.register('doPUReweighting', True,
+#    VarParsing.multiplicity.singleton,
+#    VarParsing.varType.bool,
+#    "Do pileup reweighting"
+#)
+options.parseArguments()
+
+process = cms.Process("SemiLeptanic")
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(options.MaxEvents) ) 
+
+process.source = cms.Source("EmptySource")
+process.TFileService = cms.Service("TFileService",
+	fileName = cms.string(options.outFilename) 
+	)
+
+process.BprimebH = cms.EDAnalyzer('SemiLeptanicAnalysis',
+	MaxEvents           = cms.int32(options.MaxEvents),
+	ReportEvery         = cms.int32(options.reportEvery),  
+	InputTTree          = cms.string(options.ttreedir+'/tree'),
+	InputFiles          = cms.vstring(FileNames), 
+	) 
+
+process.SimpleMemoryCheck = cms.Service("SimpleMemoryCheck",ignoreTotal = cms.untracked.int32(1) )
+process.p = cms.Path(process.Hadronic)
+
