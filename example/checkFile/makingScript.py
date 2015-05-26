42b2cc77fdf376cb18f74dc4c424e02946ec53f4
Alpha <jui-fa.tsai@cern.ch>
2015-05-26 16:20:26 +0200

    Add copy script to eos

--- example/checkFile/makingScript.py
+++ example/checkFile/makingScript.py
@@ -3,13 +3,16 @@ import os, re, sys, shutil, commands
 import math
 
 dataPath='/BprimeKitNtuples/Production_CMSSW5311/CMSSW_5_3_11_data_8TeV_22Jan2013ReReco_AOD_v4'
-#ref = open('singleElectron.txt', 'r')
-ref = open('singleMuon.txt', 'r')
+ref = open('data.txt', 'r')
+eosPath='~/eos/cms/store/user/jtsai/TTBarCPV/bprimekits/EightTeV'
+copy = open('copy.sh', 'w')
 fileName='results'
 for iref in ref:
-	rfile = iref.strip()	
+	rfile = iref.strip()
+	outfile = rfile+'.sh'
+	copy.write('cp '+outfile+' '+eosPath+'/'+rfile+'/\n')		
 	print '>> %s...'%(rfile)
-	script = open('./'+rfile+'.sh', 'w')
+	script = open(outfile, 'w')
 	listDir = commands.getoutput('ls '+dataPath+'/'+rfile+'/')
 	files = listDir.split('\n')
 	for file in files:
@@ -17,4 +20,4 @@ for iref in ref:
 			script.write('scp ntucms1.cern.ch:'+dataPath+'/'+rfile+'/'+file+' .\n')
 			script.write('sleep 5\n')
 	script.close()
-
+copy.close()		
