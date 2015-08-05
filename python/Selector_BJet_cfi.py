4c51b6ef9137b292bd83c8e8a965b6234f185cc6
Alpha <jui-fa.tsai@cern.ch>
2015-08-05 12:32:15 +0200

    Clean up tab to be space4

--- python/Selector_BJet_cfi.py
+++ python/Selector_BJet_cfi.py
@@ -3,9 +3,9 @@ import FWCore.ParameterSet.Config as cms
 from TTBarCPV.TTBarCPVAnalysisRun1.Selector_Jet_cfi import*
 
 defaultBJetSelectionParameters = defaultJetSelectionParameters.clone(
-	# jet general selections
-	jetType                  = cms.string('BJet'),
+    # jet general selections
+    jetType                  = cms.string('BJet'),
 
-	# jet b tag
-	jetCombinedSVBJetTagsMin = cms.double(0.679),
+    # jet b tag
+    jetCombinedSVBJetTagsMin = cms.double(0.679),
 )
--- python/Selector_Jet_cfi.py
+++ python/Selector_Jet_cfi.py
@@ -4,28 +4,28 @@ infiniteMax =  1000000
 infiniteMin = -1000000
 
 defaultJetSelectionParameters = cms.PSet(
-	# jet general selections
-	jetType                  = cms.string('TightJet'),
-	jetPtMin                 = cms.double(30),
-	jetPtMax                 = cms.double(infiniteMax),
-	jetAbsEtaMin             = cms.double(infiniteMin),
-	jetAbsEtaMax             = cms.double(2.4),
+    # jet general selections
+    jetType                  = cms.string('TightJet'),
+    jetPtMin                 = cms.double(30),
+    jetPtMax                 = cms.double(infiniteMax),
+    jetAbsEtaMin             = cms.double(infiniteMin),
+    jetAbsEtaMax             = cms.double(2.4),
 
-	# jet id
-	jetNConstituentsMin      = cms.double(1),
-	jetNConstituentsMax      = cms.double(infiniteMax),
-	jetCHFMin                = cms.double(0),
-	jetCHFMax                = cms.double(infiniteMax),
-	jetNCHMin                = cms.double(0),
-	jetNCHMax                = cms.double(infiniteMax),
-	jetNEFMin                = cms.double(infiniteMin),
-	jetNEFMax                = cms.double(0.99),
-	jetNHFMin                = cms.double(infiniteMin),
-	jetNHFMax                = cms.double(0.99),
-	jetCEFMin                = cms.double(infiniteMin),
-	jetCEFMax                = cms.double(0.99),
+    # jet id
+    jetNConstituentsMin      = cms.double(1),
+    jetNConstituentsMax      = cms.double(infiniteMax),
+    jetCHFMin                = cms.double(0),
+    jetCHFMax                = cms.double(infiniteMax),
+    jetNCHMin                = cms.double(0),
+    jetNCHMax                = cms.double(infiniteMax),
+    jetNEFMin                = cms.double(infiniteMin),
+    jetNEFMax                = cms.double(0.99),
+    jetNHFMin                = cms.double(infiniteMin),
+    jetNHFMax                = cms.double(0.99),
+    jetCEFMin                = cms.double(infiniteMin),
+    jetCEFMax                = cms.double(0.99),
 
-	# jet b tag
-	jetCombinedSVBJetTagsMin = cms.double(infiniteMin),
-	jetCombinedSVBJetTagsMax = cms.double(infiniteMax),
+    # jet b tag
+    jetCombinedSVBJetTagsMin = cms.double(infiniteMin),
+    jetCombinedSVBJetTagsMax = cms.double(infiniteMax),
 )
