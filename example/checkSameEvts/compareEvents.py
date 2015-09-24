02bdf3515b7f9c0983e67b09e70739cf09e9a719
Alpha <jui-fa.tsai@cern.ch>
2015-09-24 13:22:38 +0200

    Update check events

--- example/checkSameEvts/compareEvents.py
+++ example/checkSameEvts/compareEvents.py
@@ -8,6 +8,8 @@ LumiNo = 1
 EvtNo = 2
 
 fileNames = ["SingleMu_MuChannel", "SingleMu_ElChannel", "SingleElectron_MuChannel", "SingleElectron_ElChannel" ]
+#fileNames = ["SingleElectron_MuChannel", "SingleMu_MuChannel_vs_SingleElectron_MuChannel" ]
+#fileNames = ["SingleMu_MuChannel_vs_SingleElectron_MuChannel", "SingleElectron_MuChannel" ]
 fileInfos = {}
 
 print ">> [INFO] Recording events infomations"
@@ -18,11 +20,30 @@ for name in fileNames:
     lines2 = filter( lambda x: not x.startswith('#'),  ( line.strip() for line in lines1 )) # Remove # comments lines
     lines3 = filter( None, ( line.split() for line in lines2 ))                             # Split RunNo, LumiNo, EvtNo
     fileInfos[name] = lines3
+    totalsame=0
+    for info1 in fileInfos[name]:
+        same = 0
+        for info2 in fileInfos[name]:
+            if info1 == info2:
+                same += 1
+        if same > 1:
+            totalsame += 1
+            print ">> [INFO] %d count events: %s %s %s"%( same, info1[RunNo], info1[LumiNo], info1[EvtNo])
 
+    print ">> [INFO] %d events double count"%( totalsame)
 for i in range(0, len(fileNames)):
     for j in range( i+1, len(fileNames)):
 
         sameEvts=0
+        #name1 = ""
+        #name2 = ""
+        #if len(fileInfos[fileNames[i]]) < len(fileInfos[fileNames[j]]):
+        #    name1 = fileNames[i]
+        #    name2 = fileNames[j]
+        #else:
+        #    name1 = fileNames[j]
+        #    name2 = fileNames[i]
+
         name1 = fileNames[i]
         name2 = fileNames[j]
         info1 = fileInfos[name1]
