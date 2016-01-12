3e7d0ccb1cee9ed48566f8f38f01c7ad0bc0c80c
Jui-Fa Tsai <jui-fa.tsai@xern.ch>
2016-01-12 17:06:26 +0100

    For syst

--- macro/myAna/mkTemplateSyst.C
+++ macro/myAna/mkTemplateSyst.C
@@ -5,11 +5,24 @@
 
 std::string fileName="Final_histograms_SemiLeptanic.root";
 std::string systDir="Syst";
+std::string fName="../results/15Dec_LepJet_MCDATA";
+std::string hName="EvtChi2_Ht";
+std::string output=".";
+bool doFit=true;
+int rebin=20;
 int CoCH=0;
 int ElCH=1;
 int MuCH=2;
-
-void mkTemplate( std::string fName, std::string hName, std::string output=".", int rebin=1, bool doFit=true )
+void getHistStatUnc( TH1D* h, TH1D* h_u, TH1D* h_d )
+{
+    int bins=h->GetNbinsX();
+    for( int i=1; i<=bins; i++)
+    {
+        h_u->SetBinContent(i, h->GetBinContent(i)+h->GetBinError(i));
+        h_d->SetBinContent(i, h->GetBinContent(i)-h->GetBinError(i));
+    }
+}
+void mkTemplateSyst()
 {
     const int nCh=3, nSyst=2+7, nMC=2, nTune=2;
     std::string mcName[nMC]={"TTJets__", "BkgMC_TTJetsNonSemiLeptMGDecaysExcluded__"}; // sig=0, bkg=1
@@ -17,7 +30,7 @@ void mkTemplate( std::string fName, std::string hName, std::string output=".", i
     std::string tuneName[nTune]={"up", "down"};
     std::string systName[nSyst]={"", "Stat", "PU", "JER", "BTagSF", "TopPT", "elID", "muID", "muISO"};
     
-    TFile* fin[nSyst][nTune] = new TFile();
+    TFile* fin[nSyst][nTune];
     fin[0][0] = new TFile((fName+"/"+fileName).c_str()); //mean hist
     for( int s=2; s<nSyst; s++ ){
         for( int t=0; t<nTune; t++ ){
@@ -119,68 +132,4 @@ void mkTemplate( std::string fName, std::string hName, std::string output=".", i
         }
     }
     fout->Write(); 
-}
-
-void mkTemplate( TFile* f, std::string hName, std::string output=".", int rebin=1 )
-{
-    TFile* fout = new TFile((output+"/Template_"+hName+".root").c_str(), "RECREATE");
-    TH1D *h_data, *h_sigMC, *h_bkgMC;
-    TH1D *h_data_El, *h_sigMC_El, *h_bkgMC_El;
-    TH1D *h_data_Mu, *h_sigMC_Mu, *h_bkgMC_Mu;
-
-    h_data  = (TH1D*)((TH1D*)f->Get(("DATA_Electron__"+hName+"_El").c_str()))->Clone("DATA");
-    h_sigMC = (TH1D*)((TH1D*)f->Get(("TTJets__"+hName+"_El").c_str()))->Clone("SigMC");
-    h_bkgMC = (TH1D*)((TH1D*)f->Get(("BkgMC_TTJetsNonSemiLeptMGDecaysExcluded__"+hName+"_El").c_str()))->Clone("BkgMC");
-
-    h_data_El  = (TH1D*)((TH1D*)f->Get(("DATA_Electron__"+hName+"_El").c_str()))->Clone("DATA_El");
-    h_sigMC_El = (TH1D*)((TH1D*)f->Get(("TTJets__"+hName+"_El").c_str()))->Clone("SigMC_El");
-    h_bkgMC_El = (TH1D*)((TH1D*)f->Get(("BkgMC_TTJetsNonSemiLeptMGDecaysExcluded__"+hName+"_El").c_str()))->Clone("BkgMC_El");
-
-    h_data_Mu  = (TH1D*)((TH1D*)f->Get(("DATA_Muon__"+hName+"_Mu").c_str()))->Clone("DATA_Mu");
-    h_sigMC_Mu = (TH1D*)((TH1D*)f->Get(("TTJets__"+hName+"_Mu").c_str()))->Clone("SigMC_Mu");
-    h_bkgMC_Mu = (TH1D*)((TH1D*)f->Get(("BkgMC_TTJetsNonSemiLeptMGDecaysExcluded__"+hName+"_Mu").c_str()))->Clone("BkgMC_Mu");
-
-    h_data->Rebin(rebin);  h_data_El->Rebin(rebin);  h_data_Mu->Rebin(rebin);
-    h_sigMC->Rebin(rebin); h_sigMC_El->Rebin(rebin); h_sigMC_Mu->Rebin(rebin);
-    h_bkgMC->Rebin(rebin); h_bkgMC_El->Rebin(rebin); h_bkgMC_Mu->Rebin(rebin);
-    
-    fix(h_data);  fix(h_data_El);  fix(h_data_Mu);
-    fix(h_sigMC); fix(h_sigMC_El); fix(h_sigMC_Mu);
-    fix(h_bkgMC); fix(h_bkgMC_El); fix(h_bkgMC_Mu);
-
-    h_data->Add(h_data_Mu);
-    h_sigMC->Add(h_sigMC_Mu);
-    h_bkgMC->Add(h_bkgMC_Mu);
-
-    getHistStatUnc( h_data,     "DATA_Stat");
-    getHistStatUnc( h_data_El,  "DATA_El_Stat");
-    getHistStatUnc( h_data_Mu,  "DATA_Mu_Stat");
-    getHistStatUnc( h_sigMC,    "SigMC_Stat");
-    getHistStatUnc( h_sigMC_El, "SigMC_El_Stat");
-    getHistStatUnc( h_sigMC_Mu, "SigMC_Mu_Stat");
-    getHistStatUnc( h_bkgMC,    "BkgMC_Stat");
-    getHistStatUnc( h_bkgMC_El, "BkgMC_El_Stat");
-    getHistStatUnc( h_bkgMC_Mu, "BkgMC_Mu_Stat");
-
-    fout->Write(); 
-}
-void getHistStatUnc( TH1D* h, TH1D* h_u, TH1D* h_d )
-{
-    int bins=h->GetNbinsX();
-    for( int i=1; i<=bins; i++)
-    {
-        h_u->SetBinContent(i, h->GetBinContent(i)+h->GetBinError(i));
-        h_d->SetBinContent(i, h->GetBinContent(i)-h->GetBinError(i));
-    }
-}
-void getHistStatUnc( TH1D* h, std::string name )
-{
-    TH1D* h_u = (TH1D*)h->Clone((name+"up").c_str());
-    TH1D* h_d = (TH1D*)h->Clone((name+"down").c_str());
-    int bins=h->GetNbinsX();
-    for( int i=1; i<=bins; i++)
-    {
-        h_u->SetBinContent(i, h->GetBinContent(i)+h->GetBinError(i));
-        h_d->SetBinContent(i, h->GetBinContent(i)-h->GetBinError(i));
-    }
 }
