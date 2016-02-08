7353a8a264b98bf3c84bc3985a10f7baa50a6750
Alpha <jui-fa.tsai@cern.ch>
2016-02-08 19:35:27 +0100

    Update

--- test/pdfUncertaintyAna_cfg.py
+++ test/pdfUncertaintyAna_cfg.py
@@ -21,7 +21,7 @@ options.register('reportEvery', 1000,
     VarParsing.varType.int,
     "Report every N events (default is N=1000)"
     )
-options.register('ttreedir', 'pdftree',
+options.register('ttreedir', 'SemiLeptanic',
     VarParsing.multiplicity.singleton,
     VarParsing.varType.string,
     "Name of ROOT TTree dir"
@@ -36,7 +36,7 @@ options.register('MaxChi2Cut', 40,
     VarParsing.varType.float,
     "Event selection for hadronic top max of max_chi2"
     )
-options.register('PdfName', 'ct10nnlo.LHgrid',
+options.register('PdfName', 'CT10nnlo.LHgrid', # ls /cvmfs/cms.cern.ch/slc6_amd64_gcc481/external/lhapdf/5.9.1-cms4/share/lhapdf/PDFsets
     VarParsing.multiplicity.singleton,
     VarParsing.varType.string,
     "PDF method name"
@@ -56,24 +56,12 @@ process.TFileService = cms.Service("TFileService",
     fileName = cms.string(options.outFilename) 
 )
 
-MT = 173.0
-Oweight = 1.0
-power = int(options.Owrt.split(':')[1])
-i = 0
-while ( i < power ):
-    Oweight *= MT
-    i+=1
-
-isSkim = False
-if options.ttreedir.lower() == 'skim':
-    isSkim = True
-
 process.CheckEventsOfLepJets = cms.EDAnalyzer('PDFUncertaintyAna',
     MaxEvents   = cms.int32(options.MaxEvents),
     ReportEvery = cms.int32(options.reportEvery),  
-    InputTTree  = cms.string(options.ttreedir+'/root'),
+    InputTTree  = cms.string(options.ttreedir+'/pdftree'),
     InputFiles  = cms.vstring(FileNames), 
-    #InputFiles  = cms.vstring(FileNames_SemiLeptAnaTopResults), 
+    #InputFiles  = cms.vstring("root://eoscms//eos/cms/store/user/jtsai/TTBarCPV/results/08Feb_PDFTree/TTJets_SemiLeptMGDecays/SemiLeptanicAnalysis_35.root"), 
     MaxChi2Cut  = cms.double(options.MaxChi2Cut),
     MinChi2Cut  = cms.double(options.MinChi2Cut),
     PdfName     = cms.string(options.PdfName), 
